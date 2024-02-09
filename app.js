3da4d10c0b73c7e4800a3b8ffdf2ace82b2c9b8c
Furkan Çetiner <furkanctiner@gmail.com>
2024-02-09 18:22:10 +0300

    static files and referrers added

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
+
+const port = 3000
+const hostname = '127.0.0.1'
 
 const app = express();
 
-const blog = { id: 1, title: "Blog title", description: "Blog description" };
+//TEMPLATE ENGİNE
+app.set("view engine", "ejs");
 
+//MİDDLEWARES
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+//ROUTERS
 app.get("/", (req, res) => {
-  res.send(blog);
+  res.render('index');
 });
-const port = 3000;
-app.listen(port, () => {
-  console.log(`SERVER ${port} ON`);
+app.get("/about", (req, res) => {
+  res.render('about');
 });
+app.get("/add_post", (req, res) => {
+  res.render('add_post');
+}); 
+/* app.post("/photos", (req, res) => {
+  console.log(req.body);
+  res.redirect('/')
+});  */
+
+app.listen(port, hostname, () => {
+  console.log(`SERVER ON! http://${hostname}:${port}/`)
+});
+
